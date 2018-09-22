79442437699791d028e3c2d475d1f1c5f266e21e
Kiryu144 <123davidLP@gmail.com>
2018-09-22 17:50:37 +0200

    Added interface for every gameclass

---
+++ source/engine/acrylgame.h
@@ -0,0 +1,13 @@
+/* Created by David Klostermann on 22.09.2018. */
+#ifndef ACRYL2D_ACRYLGAME_H
+#define ACRYL2D_ACRYLGAME_H
+
+class Acryl2D;
+
+class AcrylGame {
+public:
+    virtual void update(double delta) {};
+};
+
+
+#endif
